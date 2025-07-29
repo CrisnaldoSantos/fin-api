c1c8791701c3ed6296db805b4ca56b5f591313a1
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2025-07-29 00:43:08 -0300

    feat: created transaction table migration

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,3 @@
 import { config } from "./src/database.ts";
 
-// export default {
-//   ...config,
-//   migrations: {
-//     directory: "./src/database/migrations",
-//   },
-//   seeds: {
-//     directory: "./src/database/seeds",
-//   },
-// };
 export default config;
